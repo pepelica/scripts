d082f710fc0d52b886f4ab6c310ddcfa04f8f583
unknown <aleksandra-korenskaya@mail.ru>
2020-09-15 22:51:18 +0700

    убираем косяк

--- collect_protein_data.py
+++ collect_protein_data.py
@@ -70,7 +70,6 @@ def merge_data(path_paxdb, path_resultsEloE, organism, folderpaxdb_list):
     med = abundance.median(axis = 1, skipna = True)
     #median = median.rename('median')
     med = pd.DataFrame(data = med, columns =['median']) #Теперь будет работать!
-    throw new Error() # работает!
     print(med)
 #    median.columns = pd.MultiIndex.from_frame(median, names = ('abundance', 'median'))
     final_data = pd.merge(final_data, med, how = 'outer', left_on = True)
